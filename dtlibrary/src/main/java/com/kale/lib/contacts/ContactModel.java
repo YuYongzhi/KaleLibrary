a4b9fcc089616d97fb2aae5d5b68f86fcd83ca39
tianzhijiexian <kai.jin@duitang.com>
2015-05-11 13:11:24 +0800

    添加了操作联系人对象的帮助类

---
+++ dtlibrary/src/main/java/com/kale/lib/contacts/ContactModel.java
@@ -0,0 +1,28 @@
+package com.kale.lib.contacts;
+
+/**
+ * @author Jack Tony
+ * @date 2015/5/11
+ */
+public class ContactModel {
+
+    private String contactName;
+
+    private String phoneNumber;
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
